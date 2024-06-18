37b409b9be1737241bcfaa594241b75ad139e181
Panagiotis Kalogeropoulos <panosbino@gmail.com>
2024-06-18 14:41:41 +0200

    Added function normalize_all

--- filtering_functions/Utils.R
+++ filtering_functions/Utils.R
@@ -7,3 +7,29 @@ import_into_sobj <- function(data_dir){
   rm(sc_data)
   return(sobj)
 }
+
+
+normalize_all <- function(sc_input_object,method = "cpm") {
+  if(class(sc_input_object) == "Seurat")
+  {sce <- Seurat::as.SingleCellExperiment(sc_input_object)}
+  else if (class(sc_input_object) == "SingleCellExperiment")
+  {sce <- sc_input_object}
+
+  if (method == "cpm") {
+    assay(sce, "normed") <- scuttle::calculateCPM(sce)
+  }
+  else if(method == "log"){
+    assay(sce, "normed") <- scuttle::normalizeCounts(sce, log=FALSE, transform = "log",size.factors=librarySizeFactors(sce), pseudo.count=1)
+  }
+  else if (method == "scran"){
+    clusters <- scran::quickCluster(sce)
+    size_factors <- scuttle::computePooledFactors(sce, clusters=clusters)
+    assay(sce, "normed") <- scuttle::normalizeCounts(sce, log=FALSE, transform = "log",size.factors=librarySizeFactors(size_factors) , pseudo.count=1)
+  }
+  else if(method == "asinh"){
+    assay(sce, "normed") <- scuttle::normalizeCounts(sce, log=FALSE, transform = "asihn",size.factors=librarySizeFactors(sce), pseudo.count=1)
+  }
+  return(Seurat::as.Seurat(sce, counts = "counts", data = "normed"))
+}
+
+
